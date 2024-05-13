d871c5fb3df36e39e81c5b984891119f0213e4e9
yashikatiwana <yashika2@sheridancollege.ca>
2024-05-13 15:15:47 -0400

    Student List is updated

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -21,11 +21,13 @@ public class StudentList {
         Student s2 = new Student();
         s2.setName("Muskan");
         Student s3 = new Student();
-        s3.setName("avleen");
+        s4.setName("avleen");
+        Student s4 = new Student();
+        s4.setName("avleen");
         StudentList[0] =s1;
         StudentList[1] =s2;
         StudentList[2] =s3;
-        
+        StudentList[3] =s4;
         
         //fill in 4 student names before running
         for (Student s : StudentList) {
